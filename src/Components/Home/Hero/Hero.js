f1c5cd3e02588e3cda50d6cb6715995e2c6cb4ab
luisgaleas-clockwork <luis.galeasjr@gmail.com>
2020-02-16 00:25:31 -0500

    Created vid hero

--- src/Components/Home/Hero/Hero.js
+++ src/Components/Home/Hero/Hero.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import HeroVid from '../../../assets/video/HeroVid.mp4'
 
 import './_hero.scss'
 
 const Hero = () => {
     return (
         <div className="hero">
-            {/* <h1>Sydney, Australia</h1>
-            <p>Welcome to the down under</p> */}
+            <video playsInline="playsinline" autoPlay="autoplay" muted="muted" loop="loop">
+                <source src={HeroVid} type="video/mp4" />
+            </video>
+            <div className="video__text">
+                <h1>Sydney, Australia</h1>
+                <p>Welcome to the down under</p>
+            </div>
         </div>
     )
 }
