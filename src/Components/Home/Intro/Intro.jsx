18afe6736e88edbca0d47950cb65b15a795ab0d6
luisgaleas-clockwork <luis.galeasjr@gmail.com>
2020-02-16 13:00:13 -0500

    Created intro text & card json

---
+++ src/Components/Home/Intro/Intro.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './_intro.scss';
+
+const Intro = () => {
+    return (
+        <div className="intro">
+            <h2>Whats does sydney have to offer?</h2>
+            <p>Sydney is a coastal metropolis whose five million residents make it the largest city in Australia and is famolus for many things.
+                 The glittering harbour, complemented by landmarks like the Sydney Opera House and the Sydney Harbour Bridge.
+                 The dozens of sun-bathed beaches features a spectacular coastline. A sophisticated food and drink scene befitting a city of this stature. But one thing you
+                 might not know about Sydney is the origin of this nickname. Sydney is more often called "The Harbour City".</p>
+        </div>
+    )
+}
+
+export default Intro;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Hero from '../Components/Home/Hero/Hero'
+import Intro from '../Components/Home/Intro/Intro';
 import '../Components/_main.scss'
 
 const Home = () => {
     return(
         <div>
             <Hero />
+            <Intro />
         </div>
     )
 }
